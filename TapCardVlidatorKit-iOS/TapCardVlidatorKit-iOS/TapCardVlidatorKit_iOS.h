4690b9fb1f470ca88d03596da042cb6abd3937d0
Tap Payments <o.rabie@tap.company>
2020-05-05 14:11:21 +0200

    1- Added the framework project. 2- Copied the original tap validation kit files

---
+++ TapCardVlidatorKit-iOS/TapCardVlidatorKit-iOS/TapCardVlidatorKit_iOS.h
@@ -0,0 +1,19 @@
+//
+//  TapCardVlidatorKit_iOS.h
+//  TapCardVlidatorKit-iOS
+//
+//  Created by Osama Rabie on 05/05/2020.
+//  Copyright © 2020 Tap Payments. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for TapCardVlidatorKit_iOS.
+FOUNDATION_EXPORT double TapCardVlidatorKit_iOSVersionNumber;
+
+//! Project version string for TapCardVlidatorKit_iOS.
+FOUNDATION_EXPORT const unsigned char TapCardVlidatorKit_iOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TapCardVlidatorKit_iOS/PublicHeader.h>
+
+
